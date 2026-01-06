04d43d9ddc197aa507ca28e65f5307af213b3c1f
msabale001 <mahisabale001@gmail.com>
2026-01-06 08:10:35 +0530

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,43 +1,18 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name                 = "abhi-eks-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
