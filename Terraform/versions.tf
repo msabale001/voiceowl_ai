6a9078c77f058ad5cb71bed5d303302e2ca91f25
msabale001 <mahisabale001@gmail.com>
2026-01-06 08:09:36 +0530

    Update versions.tf

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,30 +1,10 @@
 
 terraform {
   required_version = ">= 0.12"
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
-    }
+  required_providers{
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1.0"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 6.0"
     }
   }
 }
